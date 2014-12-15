749bf6f3b767be7b2c74333ea08149466b60341b
Chee Yeo <ckyeo.1@gmail.com>
2014-12-15 14:18:50 +0000

    Chap 6 exercises

---
+++ chap6/modules7.exs
@@ -0,0 +1,17 @@
+# convert float to string with 2 decimal places (erlang)
+
+:io.format("Float to 2 decimal places ~.2f~n", [3.141593456])
+
+# get the value of a OS environment variable (elixir)
+
+System.get_env("HOME")
+
+# Return extension component of a file name (elixir)
+Path.extname("dave/test.exs")
+
+
+# Return the process current working directory (elixir)
+System.cwd()
+
+System.cmd("date",[])
+
